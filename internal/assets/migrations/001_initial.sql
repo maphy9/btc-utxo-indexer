8aea6241e8dd6a01d0640e6536750c136f56a370
maphy9 <maphy9@gmail.com>
2025-12-26 09:25:52 +0100

    Create database tables

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -1,3 +1,21 @@
 -- +migrate Up
 
+CREATE TABLE IF NOT EXISTS users
+(
+  id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  username text UNIQUE NOT NULL,
+  password_hash text NOT NULL,
+  created_at timestamptz DEFAULT current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS addresses
+(
+  id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  address text NOT NULL,
+  user_id bigint REFERENCES users (id) ON DELETE CASCADE NOT NULL
+);
+
 -- +migrate Down
+DROP TABLE IF EXISTS addresses CASCADE;
+
+DROP TABLE IF EXISTS users CASCADE;
