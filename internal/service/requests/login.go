2afcd933b1b55328ddcfb186bc0d7f7054a41439
maphy9 <maphy9@gmail.com>
2025-12-26 17:22:01 +0100

    Add boilerplace for login handler

--- internal/service/requests/login.go
+++ internal/service/requests/login.go
@@ -1,11 +1,17 @@
 package requests
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type LoginRequest struct {
-
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 func NewLoginRequest(r *http.Request) (LoginRequest, error) {
-	return LoginRequest{}, nil
+	var request LoginRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	return request, err
 }
