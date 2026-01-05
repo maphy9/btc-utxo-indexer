157b258334edce643f384f6687cf1ef7f51155ea
maphy9 <maphy9@gmail.com>
2026-01-05 14:15:02 +0100

    Move address check to a separate middleware

--- internal/service/handlers/get_utxos.go
+++ internal/service/handlers/get_utxos.go
@@ -14,25 +14,7 @@ func GetUtxos(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	logger := helpers.Log(r)
 	db := helpers.DB(r)
-	userID := helpers.UserID(r)
 	address := chi.URLParam(r, "address")
-
-	found, err := helpers.CheckAddress(ctx, db, userID, address)
-	if err != nil {
-		logger.WithError(err).Error("failed to check the address")
-		ape.RenderErr(w, apierrors.NewApiError(
-			http.StatusInternalServerError,
-			"Failed to check the address",
-		))
-		return
-	}
-	if !found {
-		ape.RenderErr(w, apierrors.NewApiError(
-			http.StatusNotFound,
-			"Address not found",
-		))
-		return
-	}
 
 	utxos, err := db.Utxos().GetActiveByAddress(ctx, address)
 	if err != nil {
